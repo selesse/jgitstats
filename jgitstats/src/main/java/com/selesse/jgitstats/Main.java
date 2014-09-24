27bca0068796ae38faec12e1ad1ab0ab40092b20
Alex Selesse <alex@selesse.com>
2014-09-23 22:46:49 -0400

    CLI parsing!

--- jgitstats/src/main/java/com/selesse/jgitstats/Main.java
+++ jgitstats/src/main/java/com/selesse/jgitstats/Main.java
@@ -1,14 +1,13 @@
 package com.selesse.jgitstats;
 
 import com.google.common.collect.Lists;
-import com.selesse.jgitstats.cli.DocOptCli;
+import com.selesse.jgitstats.cli.CommandLine;
 import com.selesse.jgitstats.graph.DiffChart;
 import com.selesse.jgitstats.template.IndexTemplate;
 import com.selesse.jgitwrapper.Branch;
 import com.selesse.jgitwrapper.CommitDiff;
 import com.selesse.jgitwrapper.CommitDiffs;
 import org.apache.velocity.VelocityContext;
-import org.eclipse.jgit.api.errors.GitAPIException;
 import org.eclipse.jgit.diff.DiffEntry;
 import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.revwalk.RevCommit;
@@ -27,46 +26,70 @@ import java.util.Map;
 public class Main {
     private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
 
-    public static void main(String[] args) throws IOException, GitAPIException {
-        DocOptCli docOptCli = new DocOptCli(args);
-        Map<String, Object> options = docOptCli.getOptions();
+    public static void main(String[] args) throws Exception {
+        Map<CommandLine.Option, Object> options = CommandLine.getOptions(args);
 
-        LOGGER.info("Got {} from DocOpt", options);
+        LOGGER.debug("Got {} from arguments", options);
+
+        String gitPath = (String) options.get(CommandLine.Option.GIT_REPO);
+
+        boolean isValidGitPath = isValidGitRoot(gitPath);
+
+        if (!isValidGitPath) {
+            LOGGER.error("Error: {} is an invalid Git root", gitPath);
+            System.exit(1);
+        }
+
+        // This is the default branch to look at
+        String branchName = "master";
+
+        if (options.containsKey(CommandLine.Option.BRANCH_NAME)) {
+            branchName = (String) options.get(CommandLine.Option.BRANCH_NAME);
+        }
+
+        runAnalysisOnLastCommit(new File(gitPath), branchName);
     }
 
-    private void doSomething() throws Exception {
+    private static boolean isValidGitRoot(String gitPath) {
+        File gitRoot = new File(gitPath);
+        if (!gitRoot.exists() || !gitRoot.isDirectory()) {
+            return false;
+        }
+
+        File gitDirectoryInRoot = new File(gitRoot, ".git");
+        return gitDirectoryInRoot.exists() && gitDirectoryInRoot.isDirectory();
+    }
+
+    private static void runAnalysisOnLastCommit(File gitDir, String branchName) throws Exception {
         FileRepositoryBuilder builder = new FileRepositoryBuilder();
-        File gitDir = new File(".git/");
         LOGGER.info("Looking for a Git repository in {}", gitDir.getAbsolutePath());
         Repository repository = builder.setGitDir(gitDir)
                 .readEnvironment() // scan environment GIT_* variables
                 .findGitDir() // scan up the file system tree
                 .build();
 
-        String branchName = "master";
-
         Branch branch = new Branch(repository, branchName);
         List<RevCommit> commits = branch.getCommits();
-        LOGGER.info("Found {} commits on {}", commits.size(), branch);
+        LOGGER.info("Found {} commits on {}", commits.size(), branch.getName());
         LOGGER.info("Commits: {}", commits);
 
         List<CommitDiff> commitDiffList = Lists.newArrayList();
 
-        for (int i = 0; i < 1; i++) {
-            RevCommit commit = commits.get(i);
-            List<DiffEntry> diffEntries = CommitDiffs.getDiff(repository, commit);
+        if (commitDiffList.size() > 0) {
+            for (int i = 0; i < 1; i++) {
+                RevCommit commit = commits.get(i);
+                List<DiffEntry> diffEntries = CommitDiffs.getDiff(repository, commit);
 
-            for (DiffEntry diffEntry : diffEntries) {
-                CommitDiff commitDiff = new CommitDiff(repository, diffEntry);
+                for (DiffEntry diffEntry : diffEntries) {
+                    CommitDiff commitDiff = new CommitDiff(repository, diffEntry);
 
-                commitDiffList.add(commitDiff);
+                    commitDiffList.add(commitDiff);
+                }
+                createChartAndIndex(commitDiffList);
             }
-
         }
 
         repository.close();
-
-        createChartAndIndex(commitDiffList);
     }
 
     public static void createChartAndIndex(List<CommitDiff> commitDiffList) throws IOException {
---
+++ jgitstats/src/main/java/com/selesse/jgitstats/cli/CommandLine.java
@@ -0,0 +1,70 @@
+package com.selesse.jgitstats.cli;
+
+import com.google.common.collect.Maps;
+import org.docopt.Docopt;
+
+import java.util.Map;
+
+public class CommandLine {
+    private static final String DEFAULT_BRANCH = "master";
+    private static final String helpMessage =
+            "jgitstats.\n" +
+                    "\n" +
+                    "Usage:\n" +
+                    "  jgitstats " + Option.GIT_REPO + " [" + Option.BRANCH_NAME + "=name]\n" +
+                    "  jgitstats -h | --help\n" +
+                    "  jgitstats -v | --version\n" +
+                    "\n" +
+                    "Options:\n" +
+                    "  -h --help         Show this screen.\n" +
+                    "  -v --version      Show version.\n" +
+                    "  --branch=<name>   Choose branch to run on [default: master].\n";
+
+    private CommandLine() {
+    }
+
+    public static Map<Option, Object> getOptions(String[] args) {
+        Map<String, Object> options = new Docopt(helpMessage).withVersion("0.1.0").parse(args);
+
+        Map<Option, Object> optionObjectMap = Maps.newHashMap();
+
+        for (Map.Entry<String, Object> stringObjectEntry : options.entrySet()) {
+            String string = stringObjectEntry.getKey();
+            Object value = stringObjectEntry.getValue();
+
+            Option option = Option.fromString(string);
+            if (option != null) {
+                optionObjectMap.put(option, value);
+            }
+        }
+
+        return optionObjectMap;
+    }
+
+    public enum Option {
+        GIT_REPO("<git-repo>"),
+        BRANCH_NAME("--branch")
+        ;
+
+        private final String docOptString;
+
+        Option(String docOptString) {
+            this.docOptString = docOptString;
+        }
+
+        @Override
+        public String toString() {
+            return docOptString;
+        }
+
+        public static Option fromString(String string) {
+            if (string.equals("<git-repo>")) {
+                return GIT_REPO;
+            } else if (string.equals("--branch")) {
+                return BRANCH_NAME;
+            }
+            return null;
+        }
+    }
+
+}
